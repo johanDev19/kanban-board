e87032854e3566f211b8270535cb1262f4877dd4
Johan Guzman <johan.dev19@gmail.com>
2020-07-17 01:39:35 -0400

    create the cards component

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
 
-import { CardContainer } from './styles'
+import {
+  CardContainer,
+  CardTitle,
+  CardDescription,
+  CardAssigneeContainer,
+  CardAssigneeName,
+  CardDueDateContainer,
+  CardDueDate,
+} from "./styles";
 
 export default () => {
-  
   return (
     <CardContainer>
-      
+      <CardTitle>Flow Meter Measurement Errors</CardTitle>
+      <CardDescription>https://blog.gesrepair.com/</CardDescription>
+      <CardAssigneeContainer>
+        <CardAssigneeName>Longform</CardAssigneeName>
+      </CardAssigneeContainer>
+      <CardDueDateContainer>
+        <CardDueDate>09/15/2019</CardDueDate>
+      </CardDueDateContainer>
     </CardContainer>
-  )
-}
+  );
+};
