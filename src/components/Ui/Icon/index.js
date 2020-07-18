035ba0a9cc680b1d4e35cab616a706d6ee26a0bf
Johan Guzman <johan.dev19@gmail.com>
2020-07-18 10:25:09 -0400

    add proptypes to Icon components

--- src/components/Ui/Icon/index.js
+++ src/components/Ui/Icon/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-import { Icon } from "./styles";
+import { IconContainer } from "./styles";
 
-export default ({ width, src, marginRight }) => (
-  <Icon src={src} width={width} marginRight={marginRight} />
-);
+function Icon({ width, src, marginRight }) {
+  return <IconContainer src={src} width={width} marginRight={marginRight} />;
+}
+
+Icon.propTypes = {
+  width: PropTypes.number,
+  src: PropTypes.string,
+  marginRight: PropTypes.number
+};
+
+export default Icon;
