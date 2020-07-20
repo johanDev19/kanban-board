d1482ce41d5c0d8565a73f56a7ca580d58c34a89
Johan Guzman <johan.dev19@gmail.com>
2020-07-20 03:42:40 -0400

    search by tags

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,2 @@
 export const API_URL =
-  "https://gist.githubusercontent.com/johanDev19/77017ef39dd6ef662e1e5e458993a065/raw/8377c026aaa0b65c59687d622937d5b3d2447f89/kanba-fake-cards.json";
-
+  "https://gist.githubusercontent.com/johanDev19/77017ef39dd6ef662e1e5e458993a065/raw/c1ae70de7b24b8fef6763ae131de3a16779aeb29/kanba-fake-cards.json";
