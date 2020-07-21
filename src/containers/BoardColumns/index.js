8bd645ff948d4977d34c3bbd7263071e6dcb2944
Johan Guzman <johan.dev19@gmail.com>
2020-07-21 03:16:30 -0400

    refactor the drag and drop feature

--- src/containers/BoardColumns/index.js
+++ src/containers/BoardColumns/index.js
@@ -6,7 +6,6 @@ import Card from "./../../components/Card";
 import {
   handleDragOver,
   handleOnDrop,
-  handleDragStart,
 } from "./../../utils/dragAndDrop";
 import {useCardsPosition} from './../../hook/useCards'
 
@@ -16,14 +15,18 @@ const Board = ({ board }) => {
   const {changeCardPosition} = useCardsPosition();
   const [cardId, setCardId] = useState(null);
 
-  const handleDragStartWrapper = (e) => {
+  const handleDragStart = (e) => {
     setCardId(e.target.id)
-    
-    return handleDragStart(e)
   }
   
   const handleOnDropWrapper = (e) => {
     const columnId = e.target.id;
+    const multipleCards = document.querySelectorAll('[data-selected="true"]');
+
+    if(multipleCards.length > 0) {
+      return multipleCards.forEach(card => changeCardPosition(card.id, columnId))
+    }
+
     changeCardPosition(cardId, columnId)
 
     return handleOnDrop(e)
@@ -33,6 +36,7 @@ const Board = ({ board }) => {
     <BoardColumnContainer>
       {board.map((column) => (
         <Column
+          key={column._id}
           title={column.title}
           id={column._id}
           handleOnDrop={handleOnDropWrapper}
@@ -40,15 +44,15 @@ const Board = ({ board }) => {
           labelColor={column.label.color}
           label={column.cards.length}
         >
-          {column.cards.map((card, index) => (
+          {column.cards.map((card) => (
             <Card
-              key={index}
+              key={card._id}
               id={card._id}
               title={card.title}
               description={card.description}
               tag={card.tags}
               dueDate={card.dueDate}
-              handleDragStart={handleDragStartWrapper}
+              handleDragStart={handleDragStart}
               handleDragOver={handleDragOver}
             />
           ))}
--- src/utils/dragAndDrop.js
+++ src/utils/dragAndDrop.js
@@ -1,39 +1,7 @@
 export const handleOnDrop = (e) => {
   e.preventDefault();
-
-  const cards = document.querySelectorAll('[data-selected="true"]');
-
-  if (cards.length === 0) {
-    const cardId = e.dataTransfer.getData("card-reference");
-    const card = document.getElementById(cardId);
-    card.style.display = "block";
-  }
-
-  cards.forEach((card) => {
-    card.style.display = "block";
-    card.setAttribute("data-selected", "false");
-  });
 };
 
 export const handleDragOver = (e) => {
   e.stopPropagation();
-};
-
-export const handleDragStart = (e) => {
-  if (document.querySelectorAll('[data-selected="true"]').length === 0) {
-    const target = e.target;
-
-    e.dataTransfer.setData(`card-reference`, target.id);
-    setTimeout(() => {
-      target.style.display = "none";
-    }, 0);
-  }
-
-  document.querySelectorAll('[data-selected="true"]').forEach((element) => {
-    e.dataTransfer.setData(`card-reference-id-${element.id}`, element.id);
-
-    setTimeout(() => {
-      element.style.display = "none";
-    }, 0);
-  });
 };
