8d16c9c4dc83df5d8a4dd796cae92875d576bc50
Johan Guzman <johan.dev19@gmail.com>
2020-07-21 22:45:18 -0400

    remove selected filter tags when user close it

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import PropTypes from "prop-types";
 
+import { Context } from "./../../context/kanba";
 import Icon from "./../Ui/Icon";
-import { FilterContainer, Result, ItemsContainer, Item, Title } from "./styles";
+import {
+  FilterContainer,
+  Options,
+  ItemsContainer,
+  Item,
+  Title,
+} from "./styles";
 
 import filterIcon from "./../../assets/icons/filter-icon.svg";
 import checkIcon from "./../../assets/icons/check-icon.svg";
@@ -11,11 +18,20 @@ import unCheckIcon from "./../../assets/icons/uncheck-icon.svg";
 const Filter = ({ title, onFilterChange, options }) => {
   const [showOptions, setShowOptions] = useState(false);
   const [selectedOptions, setSelectedOptions] = useState([]);
+  const { dispatch } = useContext(Context);
 
-  const toggleSelectedOption = option =>
-    selectedOptions.filter(selectedOption => selectedOption !== option);
+  useEffect(() => {
+    return () => {
+      dispatch({
+        type: "REMOVE_SEARCH_VALUE",
+      });
+    };
+  }, [dispatch]);
 
-  const handleSelectedOptions = option => {
+  const toggleSelectedOption = (option) =>
+    selectedOptions.filter((selectedOption) => selectedOption !== option);
+
+  const handleSelectedOptions = (option) => {
     let selectedOptionsMutated = selectedOptions;
 
     if (selectedOptions.includes(option)) {
@@ -29,15 +45,27 @@ const Filter = ({ title, onFilterChange, options }) => {
     return setSelectedOptions([...selectedOptionsMutated]);
   };
 
+  const toggleOptions = () => {
+    if (!showOptions === false) {
+      setSelectedOptions([]);
+
+      dispatch({
+        type: "REMOVE_FILTER_VALUE",
+      });
+    }
+
+    setShowOptions(!showOptions);
+  };
+
   return (
     <FilterContainer>
-      <Title onClick={() => setShowOptions(!showOptions)}>
+      <Title onClick={toggleOptions}>
         <Icon width={18} marginRight={9} src={filterIcon} />
         {title}
       </Title>
 
       {showOptions && (
-        <Result>
+        <Options>
           <ItemsContainer>
             {options.map((option, index) => (
               <Item key={index} onClick={() => handleSelectedOptions(option)}>
@@ -52,20 +80,20 @@ const Filter = ({ title, onFilterChange, options }) => {
               </Item>
             ))}
           </ItemsContainer>
-        </Result>
+        </Options>
       )}
     </FilterContainer>
   );
 };
 
 Filter.defaultProps = {
-  options: []
+  options: [],
 };
 
 Filter.propType = {
   title: PropTypes.string.isRequired,
   onFilterChange: PropTypes.func,
-  options: PropTypes.array
+  options: PropTypes.array,
 };
 
 export default Filter;
